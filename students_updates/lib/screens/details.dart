5ded7b755ef39da4bfa92e3b8bda6e088ec575f0
akhil raj <akhilraj20801@gmail.com>
2023-11-13 18:36:09 +0530

    Student_updates

---
+++ students_updates/lib/screens/details.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+//import 'dart:io';
+
+class DetailsPage extends StatefulWidget {
+  final String name;
+  final int contact;
+  final String place;
+  final String? imagePath;
+
+  const DetailsPage({
+    super.key,
+    required this.name,
+    required this.contact,
+    required this.place,
+    this.imagePath,
+  });
+  @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
